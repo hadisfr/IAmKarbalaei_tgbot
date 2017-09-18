792f2698dff51f96ca251f73e29df82056fcbaae
h@di <hadisafari76@gmail.com>
2017-09-19 01:21:22 +0430

    primary version

---
+++ __main__.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3
+
+
+import os
+from sys import stderr
+from io import BytesIO
+import json
+
+import telebot
+from PIL import Image
+
+from config import *
+
+
+class Ui(object):
+    """user interface message provider"""
+
+    def __init__(self, lang = "fa"):
+        super(Ui, self).__init__()
+        self.lang = lang
+        with open(ui_json_addr) as f:
+            self.db = json.loads(f.read())
+        if(self.lang not in self.db.keys()):
+            raise ValueError("%s language not found in %s." % (lang, ui_json_addr))
+
+    def get_message(self, key):
+        return self.db[self.lang][key]
+
+
+bot = telebot.TeleBot(TOKEN, num_threads = 3)
+ui = Ui(lang)
+
+
+@bot.message_handler(commands = ['start'])
+def msghndlr_welcome(msg):
+    chat_id = msg.chat.id
+    bot.send_message(chat_id, ui.get_message("welcome"))
+    start(chat_id)
+
+
+@bot.message_handler(regexp = ui.get_message("use_profile_photo_btn"))
+def msghndlr_use_profile_photo(msg):
+    chat_id = msg.chat.id
+    try:
+        send_photo(chat_id, bot.get_user_profile_photos(msg.from_user.id, offset = 0, limit = 1).photos[0])
+    except IndexError:
+        bot.send_message(chat_id, "use_profile_photo_err", reply_markup = telebot.types.ReplyKeyboardRemove())
+        start(chat_id)
+
+
+@bot.message_handler(content_types = ["photo"])
+def msghndlr_use_uploaded_photo(msg):
+    chat_id = msg.from_user.id
+    send_photo(chat_id, msg.photo)
+
+
+def start(chat_id):
+    keyboard_markup = telebot.types.ReplyKeyboardMarkup(one_time_keyboard = True, resize_keyboard = True)
+    keyboard_markup.add(telebot.types.KeyboardButton(ui.get_message("use_profile_photo_btn")))
+    bot.send_message(chat_id, ui.get_message("give_photo"), reply_markup = keyboard_markup)
+
+
+def send_photo(chat_id, source_photos):
+    chosen_source_photo = None
+    for p in source_photos:
+        if(p.width >= source_photo_size[0] and p.height >= source_photo_size[1]):
+            chosen_source_photo = p
+            break
+    if not chosen_source_photo:
+        chosen_source_photo = source_photos[-1]
+    
+    source_stream = BytesIO(bot.download_file(bot.get_file(chosen_source_photo.file_id).file_path))
+    source_photo = Image.open(source_stream)
+    source_photo.load()
+    source_stream.close()
+
+    photo = Image.open(templat_addr)
+    photo.load()
+
+    mask = Image.open(mask_addr)
+    mask.load()
+    mask = mask.resize(source_photo_size)
+
+    photo.paste(source_photo.resize(source_photo_size), source_photo_position, mask)
+
+    photo_stream = BytesIO()
+    photo.save(photo_stream, format = "png")
+    photo_stream.seek(0)
+    bot.send_chat_action(chat_id, "upload_photo")
+    bot.send_photo(chat_id, photo_stream)
+    photo_stream.close()
+
+    start(chat_id)
+
+
+def main():
+    bot.polling()
+
+
+if __name__ == '__main__':
+    main()
---
+++ config.py
@@ -0,0 +1,9 @@
+from keys import *
+
+
+lang = "fa"
+source_photo_size = (640, 640)
+source_photo_position = (43, 485)
+ui_json_addr = "ui.json"
+templat_addr = "prof.png"
+mask_addr = "mask.png"
